ee702d27ddca04a57cb3e0fa2255e51ddaf43ba7
lazarodu <lazarodu@gmail.com>
2025-09-15 11:56:34 -0300

    first commit

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,5 @@ module.exports = {
     'src/**/*.ts', // Inclui todos os arquivos .ts dentro da pasta src
     '!src/**/*.test.ts', // Exclui todos os arquivos .test.ts
     '!src/**/*.spec.ts'  // Exclui todos os arquivos .spec.ts
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,  // Exige 80% de cobertura de "branches" (ifs, switches, etc.)
-      functions: 80, // Exige 80% de cobertura de funções
-      lines: 80,     // Exige 80% de cobertura de linhas
-      statements: 80 // Exige 80% de cobertura de "statements"
-    }
-  }
+  ]
 };
